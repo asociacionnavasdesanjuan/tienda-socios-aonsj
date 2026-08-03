0dc0f14df98e8017231e39928392ba1efdc71fd8
asociacionnavasdesanjuan <asociacionornitologicanavasdesanjuan@hotmail.com>
2026-08-03 04:30:18 +0200

    Add product management to shopping cart

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+let carrito = [];
 
+function añadirProducto(nombre, precio){
+
+    carrito.push({
+        nombre:nombre,
+        precio:precio
+    });
+
+    alert(nombre + " añadido al carrito");
+
+    console.log(carrito);
+
+}
